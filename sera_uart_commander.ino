c7cda0582209e3b38db218b2a37679e1df430ec1
Răzvi <38797478+PlasmaStorm7@users.noreply.github.com>
2022-03-20 13:54:56 +0200

    Add files via upload

--- sera_uart_commander.ino
+++ sera_uart_commander.ino
@@ -8,42 +8,36 @@ DS3231 clock;
 
 bool h12Flag;
 bool pmFlag;
-int dayTime=0;
-int previousDT=1;
+bool dayTime = 0;
 
-int isDayTime(int hour){
-  if(hour>=10 && hour<16)
-    return 1;
-  else 
-    return 0;
+bool isDayTime(int hour) {
+  if (hour >= 9 && hour < 17)
+    return true;
+  else
+    return false;
 }
 
 void setup() {
-	// Start the I2C interface
-	Wire.begin();
- 
-	// Start the serial interface
-	Serial.begin(9600);
- previousDT=!dayTime;
+  // Start the I2C interface
+  Wire.begin();
+
+  // Start the serial interface
+  Serial.begin(9600);
 }
 
 void loop() {
-	
-  
-	
+
+
+
   //Serial.println(clock.getHour(h12Flag, pmFlag)); //prints the hour
-	dayTime=isDayTime(clock.getHour(h12Flag, pmFlag)); //evaluates if datime by checking hour
-      
-  	if(!(dayTime==previousDT)){ //only change if it's not already
-      if(dayTime){
-        Serial.print("S:20.0");   //set temp to 20 degrees in daytime     
-      }
-      else
-      {
-        Serial.print("S:16.0"); //set temp to 16 degrees while nighttime
-      }
-  	}
-   previousDT=dayTime;
-   delay(1200000);//20 minutes until next check
-	
+  dayTime = isDayTime(clock.getHour(h12Flag, pmFlag));  //evaluates if datime by checking hour
+
+
+  if (dayTime) {
+    Serial.print("S:20.0");  //set temp to 20 degrees in daytime
+  } else {
+    Serial.print("S:11.0");  //set temp to 11 degrees while nighttime
+  }
+
+  delay(1200000);  //20 minutes until next check
 }
